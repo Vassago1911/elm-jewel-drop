bbfda9698377c939e7f292a9256b7ca3f6261daa
thorti <losthortos@directbox.com>
2019-04-22 16:42:35 +0200

    refactored points to use geometry-library

--- src/Main.elm
+++ src/Main.elm
@@ -4,32 +4,73 @@ import Html exposing (Html)
 import Html.Attributes
 import Svg
 import Svg.Attributes
+import Browser
+import Browser.Events 
 
+import Point2d exposing (Point2d)
+import Vector2d 
+
+main : Program () Model Msg 
 main =
-    view init
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
     
 type alias Model = 
     { lines : List Line
-    , marble : Point
+    , marble : Point2d
     }
 
 type Line
-    = Line Point Point
+    = Line Point2d Point2d
 
-type alias Point =
-    { x : Float
-    , y : Float
-    }
+init : flags -> ( Model, Cmd msg )
+init _ =
+    ( { lines = 
+            [ Line 
+                (Point2d.fromCoordinates ( 0, 0 )) 
+                (Point2d.fromCoordinates ( 0, 1920 )) 
+            , Line 
+                (Point2d.fromCoordinates ( 0, 1920 )) 
+                (Point2d.fromCoordinates ( 1280, 1920 ))
+            , Line 
+                (Point2d.fromCoordinates ( 1280, 1920 )) 
+                (Point2d.fromCoordinates ( 1280, 0 ))
+            ]
+      , marble = 
+            Point2d.fromCoordinates ( 640, 0 )
+      }
+    , Cmd.none 
+    )
 
-init : Model
-init =
-    { lines = 
-        [ Line { x = 0, y = 0 } { x = 0, y = 1920 }
-        , Line { x = 0, y = 1920 } { x = 1280, y = 1920 }
-        , Line { x = 1280, y = 1920 } { x = 1280, y = 0 }
-        ]
-    , marble = { x = 640, y = 0 }
-    }
+
+type Msg 
+    = Tick Float
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick delta -> 
+            ( { model | marble = moveDown model.lines delta model.marble}
+            , Cmd.none
+            )
+
+
+
+moveDown : List Line -> Float -> Point2d -> Point2d
+moveDown lines delta point =
+    let 
+        displacement =
+            Vector2d.fromComponents ( 0, delta )
+    in
+    Point2d.translateBy displacement point
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Browser.Events.onAnimationFrameDelta Tick
 
 view : Model -> Html msg
 view model =
@@ -42,8 +83,8 @@ view model =
         [ Svg.g [] 
             (List.map renderline model.lines)
         , Svg.circle 
-            [ Svg.Attributes.cx (String.fromFloat model.marble.x)
-            , Svg.Attributes.cy (String.fromFloat model.marble.y)
+            [ Svg.Attributes.cx (String.fromFloat (Point2d.xCoordinate model.marble))
+            , Svg.Attributes.cy (String.fromFloat (Point2d.yCoordinate model.marble))
             , Svg.Attributes.r "20"
             ] [] 
         ]
@@ -52,10 +93,10 @@ view model =
 
 renderline (Line start end) =
     Svg.line
-        [ Svg.Attributes.x1 (String.fromFloat start.x)
-        , Svg.Attributes.y1 (String.fromFloat start.y)
-        , Svg.Attributes.x2 (String.fromFloat end.x)
-        , Svg.Attributes.y2 (String.fromFloat end.y)
+        [ Svg.Attributes.x1 (String.fromFloat (Point2d.xCoordinate start))
+        , Svg.Attributes.y1 (String.fromFloat (Point2d.yCoordinate start))
+        , Svg.Attributes.x2 (String.fromFloat (Point2d.xCoordinate end))
+        , Svg.Attributes.y2 (String.fromFloat (Point2d.yCoordinate end))
         , Svg.Attributes.strokeWidth "2"
         , Svg.Attributes.stroke "black"
         ]
